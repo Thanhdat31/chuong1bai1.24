9aab82a8668f02b05c5a62c24e4f49015da75932
Thanhdat31 <125192189+Thanhdat31@users.noreply.github.com>
2023-02-28 19:27:27 +0700

    Add files via upload

---
+++ bai1.24.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+using namespace std;
+struct NGAY {
+	int ngay, thang, nam;
+};
+struct HOPSUA {
+	char nhanhieu[20];
+	float trongluong;
+	NGAY hansudung;
+};
+void nhapsolieu(HOPSUA& hs)
+{
+	cout << "nhap nhan hieu :";
+	cin.getline(hs.nhanhieu, 21);//hàm kỹ tự được xác định
+	cout << "trongluong :";
+	cin >> hs.trongluong;
+	cout << "nhap ngay het han su dung :";
+	cin >> hs.hansudung.ngay;
+	cout << "nhap thang het han su dung :";
+	cin >> hs.hansudung.thang;
+	cout << "nhap nam su dung :";
+	cin >> hs.hansudung.nam;
+	cin.ignore();// hàm loại bỏ ký tự xuống dòng còn lại trong bộ nhớ đệm trước khi sử dụng hàm cin.getline().
+}
+void xuatsolieu(HOPSUA& hs)
+{
+	cout << "nhan hieu la :" << hs.nhanhieu << endl;
+	cout << "trong luong gam :" << hs.trongluong << endl;
+	cout << "ngay het hang la :" << hs.hansudung.ngay << "/" << hs.hansudung.thang << "/" << hs.hansudung.nam << endl;
+		;
+}
+int main()
+{
+	HOPSUA hs;
+	nhapsolieu(hs);
+	xuatsolieu(hs);
+	return 0;
+}
